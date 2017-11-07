31d8d8f88296e82569c65eb83c7c7942a9db4a56
Guillaume Ferron <guillaumef.22@gmail.com>
2017-11-07 11:36:33 +0100

    New resquests handling
    Possible to get all temperatures, only the latest, a specific day month or year one, or the ones ulterior to a given date.
    Furthermore, the date format is now aaaa-mm-dd-hh-mm-ss

--- controllers/temperature.js
+++ controllers/temperature.js
@@ -3,9 +3,123 @@
  **/
 import Temperature from '../models/temperature'
 
-export const temperature = (req, res, next) => {
-    Temperature.find({}, {'temperature': 1, _id: 0}).sort({_id:-1}).limit(1).exec((err, temperatures) => res.json(
-
+//All temperatures
+export const allTemperatures = (req, res, next) => {
+    Temperature.find({}, {}).exec((err, temperatures) => res.json(
         { temperature: temperatures}
     ));
+};
+
+//Last Temperature
+export const lastTemperature = (req, res, next) => {
+    Temperature.find({}, {}).sort({_id:-1}).limit(1).exec((err, temperatures) => res.json(
+        { temperature: temperatures}
+    ));
+};
+
+//Temperatures ulterior to a given date
+export const periodTemperature = (req, res, next) => {
+    Temperature.find({}, {}).exec(function(err, temperatures) {
+        let sortedTemp = [];
+        const period = req.params.date.split("-");
+
+        //Go through all temperatures in db
+        for(let i = 0; i < temperatures.length; i++) {
+            let curr = temperatures[i].date.split("-");
+            let verif = true;
+            //Check that the two formats are correct
+            if(curr.length === period.length) {
+                //Go through all fields of date
+                for (let j = 0; j < curr.length; j++) {
+                    //If the current field is lower than the one given as a url parameter, then it is anterior
+                    if(parseInt(curr[j]) < parseInt(period[j])) {
+                        verif = false;
+                        break;
+                    }
+                }
+                if(verif) {
+                    sortedTemp.push(temperatures[i]);
+                }
+            }
+        }
+        return res.json({ temperature: sortedTemp}
+    )});
+};
+
+//Specific day temperature
+export const dayTemperature = (req, res, next) => {
+    Temperature.find({}, {}).exec(function(err, temperatures) {
+        let sortedTemp = [];
+        const period = req.params.date.split("-");
+
+        //Go through all temperatures in db
+        for(let i = 0; i < temperatures.length; i++) {
+            let curr = temperatures[i].date.split("-");
+            let verif = true;
+            //Check that the two formats are correct
+            if(curr.length === period.length) {
+                //Go through athe first three fields of date, hence the given day
+                for (let j = 0; j < 3; j++) {
+                    //If the current field is lower than the one given as a url parameter, then it is anterior
+                    if(parseInt(curr[j]) !== parseInt(period[j])) {
+                        verif = false;
+                        break;
+                    }
+                }
+                if(verif) {
+                    sortedTemp.push(temperatures[i]);
+                }
+            }
+        }
+        return res.json({ temperature: sortedTemp}
+        )});
+};
+
+//Specific month temperature
+export const monthTemperature = (req, res, next) => {
+    Temperature.find({}, {}).exec(function(err, temperatures) {
+        let sortedTemp = [];
+        const period = req.params.date.split("-");
+
+        //Go through all temperatures in db
+        for(let i = 0; i < temperatures.length; i++) {
+            let curr = temperatures[i].date.split("-");
+            let verif = true;
+            //Check that the two formats are correct
+            if(curr.length === period.length) {
+                //Go through athe first two fields of date, hence the given month
+                for (let j = 0; j < 2; j++) {
+                    //If the current field is lower than the one given as a url parameter, then it is anterior
+                    if(parseInt(curr[j]) !== parseInt(period[j])) {
+                        verif = false;
+                        break;
+                    }
+                }
+                if(verif) {
+                    sortedTemp.push(temperatures[i]);
+                }
+            }
+        }
+        return res.json({ temperature: sortedTemp}
+        )});
+};
+
+//Specific year temperature
+export const yearTemperature = (req, res, next) => {
+    Temperature.find({}, {}).exec(function(err, temperatures) {
+        let sortedTemp = [];
+        const period = req.params.date.split("-");
+
+        //Go through all temperatures in db
+        for(let i = 0; i < temperatures.length; i++) {
+            let curr = temperatures[i].date.split("-");
+            //Check that the two formats are correct
+            if(curr.length === period.length) {
+                if(parseInt(curr[0]) === parseInt(period[0])) {
+                    sortedTemp.push(temperatures[i]);
+                }
+            }
+        }
+        return res.json({ temperature: sortedTemp}
+        )});
 };
--- router.js
+++ router.js
@@ -4,12 +4,27 @@
 
 import express, { Router } from 'express';
 // Import index action from movies controller
-import { temperature } from './controllers/temperature';
+import * as TempController from './controllers/temperature';
 
 // Initialize the router
 const router = Router();
 
-router.route('/temperature')
-    .get(temperature);
+//All temperatures
+router.route('/temperature/all').get(TempController.allTemperatures);
+
+//Last Temperature
+router.route('/temperature/last').get(TempController.lastTemperature);
+
+//Temperatures ulterior to a given date
+router.route('/temperature/period/:date').get(TempController.periodTemperature);
+
+//Specific day temperature
+router.route('/temperature/day/:date').get(TempController.dayTemperature);
+
+//Specific month temperature
+router.route('/temperature/month/:date').get(TempController.monthTemperature);
+
+//Specific month temperature
+router.route('/temperature/year/:date').get(TempController.yearTemperature);
 
 export default router;
