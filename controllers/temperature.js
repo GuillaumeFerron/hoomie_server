7cdc57066b8b847d6417075bb361968abfdbb42d
ewilys <lisa.martini@insa-lyon.fr>
2018-01-09 14:13:55 +0100

    find issue

--- controllers/temperature.js
+++ controllers/temperature.js
@@ -210,7 +210,7 @@ export const addTemp = (req,res,next) => {
             }
             async.series([
                 function (callback) {
-                    createTemp(d.date, t, r, callback);
+                    createTemp(d.date,d.value, r, callback);
                     t = parseFloat(d.value);
                     if(!isNaN(t)){
                         //if(d.date.match(/\d{4}((-)\d{2}){5}/))
